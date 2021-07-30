389e112f340a50ed06c26b573648677503bbfbac
Al-Hussein Abutaleb <alhussein.abutaleb@gmail.com>
2021-07-30 09:54:05 +0100

    fixed the issue with the referencing of the image

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import Chat from './components/Chat';
 import Event from './components/Event';
 import Header from './components/Header';
-import convStructureImage from 'D:/MSc AI and ML/CS5917 MSc Project in AI/My project/frontend/src/conversational_structure_chameleon_27072021.png';
 import { BrowserRouter } from 'react-router-dom';
 import React, { useState } from 'react';
 import axios from 'axios';
@@ -115,7 +114,7 @@ function App() {
             <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
             </p>
 
-            <img className="conversation-flow" src={convStructureImage} alt="Write something here" />
+            <img className="conversation-flow" src={process.env.PUBLIC_URL + 'conversational_structure_chameleon_27072021.png'} alt="Write something here" />
           </div>
 
         </div>
